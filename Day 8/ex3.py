848630e1e3e734f9b4b47cfbfb53cba920193350
nunodxxd <baptistanuno.game@gmail.com>
2022-12-02 18:04:46 +0000

    Day 8 exercises - ex 3 - better solution for other lists

--- Day 8/ex3.py
+++ Day 8/ex3.py
@@ -1,17 +1,20 @@
 from art import logo
 print(logo)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
+  alphabet_cp = alphabet.copy()
+  for x in range(shift_amount):
+      alphabet_cp += alphabet_cp.pop(0)
+
   end_text = ""
   if cipher_direction == "decode":
     shift_amount *= -1
   for char in start_text:
     if char in alphabet:
       position = alphabet.index(char)
-      new_position = position + shift_amount
-      end_text += alphabet[new_position]
+      end_text += alphabet_cp[position]
     else:
       end_text += char
   print(f"Here's the {cipher_direction}d result: {end_text}")
