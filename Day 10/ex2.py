abd49bc6392f5071b4ab875c53100b822da0799d
nunodxxd <baptistanuno.game@gmail.com>
2022-12-04 21:19:27 +0000

    Day 10 exercises

---
+++ Day 10/ex2.py
@@ -0,0 +1,44 @@
+from art import logo
+
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(n1,n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    print(logo)
+
+    num1 = float(input("What's the fisrt number?: "))
+    for x in operations:
+        print(x)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        operation = operations[operation_symbol]
+        answer = operation(num1,num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
